0e30947c5c98f99b4f4b4a02b2a1dbaa95c7729a
devdamo <minecraft.pop11@outlook.com>
2025-02-05 20:51:39 +0000

    Added a few things
    Do not run this site as its untested and dose not work

---
+++ src/scripts.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('cartDropdownButton5').click();
+});
+
+// Toggle Mobile Menu
+const hamburgerBtn = document.getElementById('hamburgerBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+hamburgerBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
+
+// Toggle Desktop Account Dropdown
+const userMenuButton = document.getElementById('userMenuButton');
+const userMenu = document.getElementById('userMenu');
+userMenuButton.addEventListener('click', () => {
+    userMenu.classList.toggle('hidden');
+});
+
+// Toggle Mobile Account Dropdown
+const mobileUserMenuButton = document.getElementById('mobileUserMenuButton');
+const mobileUserMenu = document.getElementById('mobileUserMenu');
+mobileUserMenuButton.addEventListener('click', () => {
+    mobileUserMenu.classList.toggle('hidden');
+});
+
+
+
+
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+        // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+
+});
